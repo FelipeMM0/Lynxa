5f9bd478115650d825c8a093fd26500b919a5fef
FelipeMM0 <felipemendescp720@gmail.com>
2025-06-12 23:47:01 -0300

    feat(layout): add Footer component with supplier

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import supplier from '@/assets/logo-aurora.svg'
+export function Footer() {
+  return (
+    <footer className="w-full text-center">
+      <p className="text-sm text-muted-foreground">Powered by</p>
+      <div className="w-full max-w-4xl mx-auto flex justify-center items-center opacity-70">
+        <img src={supplier} />
+      </div>
+    </footer>
+  )
+}
